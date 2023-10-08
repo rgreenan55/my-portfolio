b34329d8021b7439758ce53b36f675abcadd7845
rgreenan55 <rjgreenan55@gmail.com>
2023-10-08 17:33:21 -0300

    Added Home Stuff

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,33 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import { motion } from 'framer-motion';
+
 import PageAnimation from './PageAnimation.js';
+import { Box, Button, Grid, Stack, Typography } from '@mui/material';
+import EastIcon from '@mui/icons-material/East';
 
 const Home = () => {
+    const navigate = useNavigate();
 
     return (
         <PageAnimation>
-            Hello Home
+            <Grid container justifyContent='center' alignItems='center' pt='15%'>
+                <Stack spacing={4}>
+                    <motion.div animate={{ scale: [1, 2, 1]}} transition={{ delay: 0.5 }}>
+                        <Typography variant='h2' align='center'> Welcome </Typography>
+                    </motion.div>
+                    <motion.div>
+                        <Typography variant='h4' align='center'> My name is Robert</Typography>
+                    </motion.div>
+                    <motion.div whileHover={{ scale: 1.25 }}>
+                        <Box display='flex' justifyContent='center'>
+                            <Button variant='contained' size='large' endIcon={<EastIcon />} onClick={() => navigate('/about')}>
+                                About Me
+                            </Button>
+                        </Box>
+                    </motion.div>
+                </Stack>
+            </Grid>
         </PageAnimation>
     )
 }
